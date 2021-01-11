e10dda212fa3644ba90cd43f71d96bd175ddb4c0
sunny335 <ahmedsunny868@gmail.com>
2021-01-11 15:41:14 +0600

    feat: header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button, Col, Container, Nav, Navbar, NavItem, Row } from 'reactstrap';
+import logo from 'src/assets/image/logo.png';
+
+const Header = () => {
+  return (
+    <header className="border-bottom shadow-sm py-4">
+      <Container>
+        <Row>
+          <Col lg={3} className="d-flex">
+            <img src={logo} alt="logo" />
+            <h3 className="mb-0">
+              <span className="text-primary">Exel</span>Stock
+            </h3>
+          </Col>
+          <Col lg={6}>
+            <Navbar className="p-0 d-block">
+              <Nav className="justify-content-end">
+                <NavItem>
+                  <NavLink
+                    exact
+                    className="nav-link text-capitalize text-secondary"
+                    to="/"
+                  >
+                    Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    exact
+                    className="nav-link text-capitalize text-secondary"
+                    to="/about"
+                  >
+                    About Us
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    exact
+                    className="nav-link text-capitalize text-secondary"
+                    to="/valus"
+                  >
+                    Valus
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    exact
+                    className="nav-link text-capitalize text-secondary"
+                    to="/contact"
+                  >
+                    Contact Us
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Navbar>
+          </Col>
+          <Col
+            className="text-end align-self-center justify-content-around d-flex"
+            lg={3}
+          >
+            <Button className="rounded-pill px-4" color="primary">
+              Login
+            </Button>
+            <Button className="rounded-pill px-4" color="primary">
+              Back To office
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
