d1fc83371927785f3d22100ae4b2fa7f085b9fcc
sunny335 <ahmedsunny868@gmail.com>
2021-01-11 16:02:35 +0600

    fix: chose us row colmn

--- src/views/Home/Testament/index.js
+++ src/views/Home/Testament/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Col, Row } from 'reactstrap';
 import TitleComponent from 'src/components/TitleFirst';
+
 const TitleFirst = () => {
   return (
-    <>
-      <TitleComponent title="Why Chose Us" />
-      <p className="text-center  text-secondary testamonytext">
-        That is another testament to the fact that investing without an
-        education and research will ultimaely ledebts that seem sensible during
-        periods of prosperity. When a crisis comes, individuals,companies and
-        even governments that run up debts during the boom usually suffer the
-        most.
-      </p>
-      <div className="d-flex justify-content-center my-4">
-        <div className="textbottomboder  me-2 "></div>
-        <div className="textbottomboder ms-2"></div>
-      </div>
-    </>
+    <Row>
+      <Col lg={12}>
+        <TitleComponent title="Why Chose Us" />
+        <p className="text-center  text-secondary testamonytext">
+          That is another testament to the fact that investing without an
+          education and research will ultimaely ledebts that seem sensible
+          during periods of prosperity. When a crisis comes,
+          individuals,companies and even governments that run up debts during
+          the boom usually suffer the most.
+        </p>
+        <div className="d-flex justify-content-center my-4">
+          <div className="textbottomboder  me-2 " />
+          <div className="textbottomboder ms-2" />
+        </div>
+      </Col>
+    </Row>
   );
 };
 
