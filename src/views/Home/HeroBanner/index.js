8b4395cc8889261d05c9ea3c87a05e8cd430a32b
sabbirlog <mridhasabbir5@gmail.com>
2021-01-11 15:35:52 +0600

    feat: hero-banner images

--- src/views/Home/HeroBanner/index.js
+++ src/views/Home/HeroBanner/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Col, Container, Row } from 'reactstrap';
 
+import stockImg from 'src/assets/image/stock.png';
+import cloudImg from 'src/assets/image/cloud.png';
+
 import BannerContent from './BannerContent';
 
 const HeroBanner = () => {
   return (
-    <section className="hero-banner">
+    <section className="hero-banner position-relative">
       <Container>
         <Row>
           <Col md={4}>
             <BannerContent />
           </Col>
           <Col md={8}>
-            <img src="" alt="" />
+            <img src={stockImg} alt="stock" />
           </Col>
         </Row>
       </Container>
+      <img src={cloudImg} alt="cloud" />
     </section>
   );
 };
