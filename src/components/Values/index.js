7ed247582b5162aee6aac44f66467b24ff6f2ce4
heartsantor <heartsantor.bd@gmail.com>
2021-01-12 11:09:16 +0600

    feat: update style

--- src/components/Values/index.js
+++ src/components/Values/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import ValuesCard from './ValuesCard';
 import { Container, Row, Col } from 'reactstrap';
+import piler from 'src/assets/image/piler.png';
+import piler2 from 'src/assets/image/piler2.png';
 const Values = () => {
   return (
     <Container>
       <Row>
-        <Col md={{ size: 6, offset: 3 }}>
+        <Col md={{ size: 8, offset: 2 }}>
           <Row>
-            <Col md={6}>
+            <Col md={4}>
               <ValuesCard />
             </Col>
-            <Col md={6}>
+            <Col md={4}>
               <ValuesCard />
             </Col>
-            <Col md={6}>
+            <Col md={4}>
+              <div className="h-100 d-flex align-items-center">
+                <img className="w-100" src={piler} alt="piler" />
+              </div>
+            </Col>
+            <Col md={4}>
+              <div className="h-100 d-flex align-items-center">
+                <img className="w-100" src={piler2} alt="piler" />
+              </div>
+            </Col>
+            <Col md={4}>
               <ValuesCard />
             </Col>
-            <Col md={6}>
+            <Col md={4}>
               <ValuesCard />
             </Col>
           </Row>
