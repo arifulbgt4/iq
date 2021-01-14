77e64ce3cea67c1508f8eafcb97d13546b21729f
sabbirlog <mridhasabbir5@gmail.com>
2021-01-14 11:49:15 +0600

    feat: animated on mouse move

--- src/views/Home/HeroBanner/index.js
+++ src/views/Home/HeroBanner/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'reactstrap';
+import { useSpring, animated } from 'react-spring';
 
 import stockImg from 'src/assets/image/stock.png';
 import cloudImg from 'src/assets/image/cloud.png';
@@ -9,31 +10,102 @@ import plusImg from 'src/assets/image/plus.png';
 import eggShapeImg from 'src/assets/image/egg-shape.png';
 import triangleBlankImg from 'src/assets/image/triangle-blank.png';
 
-import BannerContent from './BannerContent';
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
+
+const translate4 = (x, y) => `translate3d(${x / 10}px, ${y / 10}px,0)`;
 
 const HeroBanner = () => {
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
   return (
-    <section className="hero-banner position-relative">
-      <img src={triangleImg} alt="triangle" className="triangle1-img" />
-      <img src={starImg} alt="star" className="star1-img" />
-      <img src={plusImg} alt="plus" className="plus1-img" />
-      <img src={eggShapeImg} alt="" className="egg-shape-img" />
-      <img src={triangleBlankImg} alt="" className="triangle-blank-img" />
+    <section
+      className="hero-banner position-relative"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+    >
+      <animated.img
+        src={triangleImg}
+        alt="triangle"
+        className="triangle1-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
+      <animated.img
+        src={starImg}
+        alt="star"
+        className="star1-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
+      <animated.img
+        src={plusImg}
+        alt="plus"
+        className="plus1-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
+
+      <animated.img
+        src={eggShapeImg}
+        alt=""
+        className="egg-shape-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
+      <animated.img
+        src={triangleBlankImg}
+        alt=""
+        className="triangle-blank-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
       <Container>
         <Row>
           <Col md={4}>
-            <BannerContent />
+            <animated.div
+              className="banner-content"
+              style={{ transform: props.xy.interpolate(translate4) }}
+            >
+              <h1 className="banner-content-heading text-capitalize">
+                the stock market
+              </h1>
+              <p className="banner-content-description text-capitalize">
+                Is filled with individuals who know the price everything,but the
+                value of nothing.
+              </p>
+            </animated.div>
           </Col>
           <Col md={8}>
-            <img src={stockImg} alt="stock" className="stock-img" />
+            <animated.img
+              src={stockImg}
+              alt="stock"
+              className="stock-img"
+              style={{ transform: props.xy.interpolate(translate4) }}
+            />
           </Col>
         </Row>
       </Container>
       <img src={cloudImg} alt="cloud" className="cloud-img" />
-      <img src={triangleImg} alt="triangle" className="triangle2-img" />
-      <img src={plusImg} alt="plus" className="plus2-img" />
-      <img src={starImg} alt="star" className="star2-img" />
-      <img src={starImg} alt="star" className="star3-img" />
+      <animated.img
+        src={triangleImg}
+        alt="triangle"
+        className="triangle2-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
+      <animated.img
+        src={plusImg}
+        alt="plus"
+        className="plus2-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
+      <animated.img
+        src={starImg}
+        alt="star"
+        className="star2-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
+      <animated.img
+        src={starImg}
+        alt="star"
+        className="star3-img"
+        style={{ transform: props.xy.interpolate(translate4) }}
+      />
     </section>
   );
 };
