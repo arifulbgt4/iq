028dceeeee0dad8c7aedc5a9c94781aebfd44e23
sunny335 <ahmedsunny868@gmail.com>
2021-01-17 17:12:58 +0600

    fix: blog card docs props

--- src/views/Documentation/docks/Theme/components/BlogCardDoc/index.js
+++ src/views/Documentation/docks/Theme/components/BlogCardDoc/index.js
@@ -3,11 +3,19 @@ import React from 'react';
 import BlogCard from 'src/components/BlogCard';
 import DocCard from 'src/views/Documentation/components/DocCard';
 
+import blogImg from 'src/assets/image/blog.png';
+
 const BlogCardDoc = () => {
   return (
     <DocCard title="blog card" id="blogCard">
       <div className="w-25">
-        <BlogCard />
+        <BlogCard
+          img={blogImg}
+          title="blog title here"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the "
+          bloggerName="arif"
+          createDate="october 30, 2019"
+        />
       </div>
     </DocCard>
   );
