f64355dcb5acd9c93d9b62cda00c40d1c8a41a16
Ariful islam <arifulbgt4@gmail.com>
2021-01-17 07:21:04 +0600

    feat: value card on doc

--- src/views/Documentation/docks/Theme/index.js
+++ src/views/Documentation/docks/Theme/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import DocMain from 'src/views/Documentation/components/DocMain';
 
 import PostCard from './components/PostCard';
+import ValuCardDoc from './components/ValuCardDoc';
 
 const Theme = () => {
-  const list = ['postCard'];
+  const list = ['postCard', 'valueCard'];
   return (
     <DocMain list={list}>
       <PostCard />
+      <ValuCardDoc />
     </DocMain>
   );
 };
