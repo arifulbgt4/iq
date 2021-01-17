b5603bd89f8954bb0bc9673a4e51282880d92605
sunny335 <ahmedsunny868@gmail.com>
2021-01-17 12:16:38 +0600

    feat: footer animation

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Container } from 'reactstrap';
 
+import canvasAnimation from 'src/utils/canvasAnimation';
 import FooterBottom from './FooterBottom';
 import FooterTop from './FooterTop';
 import FooterMain from './FooterMain';
 import Animation from './Animation';
 
 const Footer = () => {
+  const mount = useRef(null);
+
+  useEffect(() => {
+    const controls = canvasAnimation(mount?.current);
+    return controls.dispose();
+  }, [mount]);
+
   return (
-    <footer className="mt-6 overflow-hidden">
+    <footer className="mt-6 overflow-hidden position-relative">
       <Container>
         <FooterTop />
       </Container>
@@ -18,6 +26,7 @@ const Footer = () => {
         <FooterBottom />
       </Container>
       <Animation />
+      <div ref={mount} />
     </footer>
   );
 };
