26c90657fc86b70641765572e155af34f3c6a233
heartsantor <heartsantor.bd@gmail.com>
2021-01-17 19:06:49 +0600

    feat: guiding with props

--- src/components/GuidingPrinciples/index.js
+++ src/components/GuidingPrinciples/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import idea from 'src/assets/image/idea.png';
-const GuidingPrinciples = () => {
+const GuidingPrinciples = (props) => {
+  const { img, title, description, style } = props;
   return (
-    <div className="d-flex mx-2">
-      <img className="px-4" src={idea} alt="idea" />
-      <div>
-        <h1>Innovation</h1>
-        <p>
-          Innovation is the cornerstone of our business. We never settle for
-          mediocre solutions. It is in our essence to use our initiation and
-          think unconventionally. We undertake careful analysis, research, and
-          develop custom solutions that not only resolve your challenges, but
-          also project you to be remarkable.
-        </p>
+    <div className={`mx-2 d-flex ${style} mb-5`}>
+      <div className="d-flex  h-100 w-75">
+        <img className="px-4" src={img} alt="idea" />
+        <div>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
--- src/views/Documentation/docks/Theme/components/GuidingPrinciplesDoc/index.js
+++ src/views/Documentation/docks/Theme/components/GuidingPrinciplesDoc/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import DocCard from 'src/views/Documentation/components/DocCard';
 import GuidingPrinciples from 'src/components/GuidingPrinciples';
+const data = [
+  {
+    id: 1,
+    style: '',
+    img: 'https://antor.thememaster.net/AllImage/goal.png',
+    title: 'Excellence',
+    description:
+      'Good is not enough; we strive to be excellent in all we do. We seek to be differentiated and original in our thinking. We focus on achieving superior risk-adjusted-performance.',
+  },
+  {
+    id: 2,
+    style: 'justify-content-end',
+    img: 'https://antor.thememaster.net/AllImage/united.png',
+    title: 'Innovation',
+    description:
+      'Innovation is the cornerstone of our business. We never settle for mediocre solutions. It is in our essence to use our initiation and think unconventionally. We undertake careful analysis, research, and develop custom solutions that not only resolve your challenges, but also project you to be remarkable.',
+  },
+  {
+    id: 3,
+    style: '',
+    img: 'https://antor.thememaster.net/AllImage/handshake.png',
+    title: 'Innovation',
+    description:
+      'Innovation is the cornerstone of our business. We never settle for mediocre solutions. It is in our essence to use our initiation and think unconventionally. We undertake careful analysis, research, and develop custom solutions that not only resolve your challenges, but also project you to be remarkable.',
+  },
+  {
+    id: 2,
+    style: 'justify-content-end',
+    img: 'https://antor.thememaster.net/AllImage/idea.png',
+    title: 'Innovation',
+    description:
+      'Innovation is the cornerstone of our business. We never settle for mediocre solutions. It is in our essence to use our initiation and think unconventionally. We undertake careful analysis, research, and develop custom solutions that not only resolve your challenges, but also project you to be remarkable.',
+  },
+];
 const GuidingPrinciplesDoc = () => {
   return (
     <DocCard title="Guiding Principles" id="GuidingPrinciples">
-      <GuidingPrinciples />
+      {data && data.map((item, i) => <GuidingPrinciples key={i} {...item} />)}
     </DocCard>
   );
 };
