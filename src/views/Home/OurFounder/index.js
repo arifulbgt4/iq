b42cf1f2872beab741c9cedb3a921921e23de644
heartsantor <heartsantor.bd@gmail.com>
2021-01-18 12:04:06 +0600

    feat: add founder style

--- src/views/Home/OurFounder/index.js
+++ src/views/Home/OurFounder/index.js
@@ -7,30 +7,34 @@ const OurFounder = () => {
   return (
     <Row className="align-items-center">
       <Col md={12}>
-        <h3 className="text-center text-primary mb-0">
+        <h1 className="text-center text-primary mb-0">
           Few Words From Our Founder
-        </h3>
+        </h1>
       </Col>
       <Col md={4}>
         <img className="img-fluid" src={founder} alt="founder" />
       </Col>
       <Col md={8}>
         <div className="text-primary">
-          <h1 className="mt-5">"</h1>
-          <p className="m-0 mt-4">
+          {/* <h1 className="mt-0">"</h1> */}
+          <img
+            className="img-cortetion"
+            src="https://antor.thememaster.net/AllImage/cotetion.png"
+            alt="a"
+          />
+          <p className="m-0  info-text">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's Lorem Ipsum is simply
             dummy text of the printing and typesetting industry. Lorem Ipsum has
             been the industry's
           </p>
-          <h6>-Narotam</h6>
+          <h5 className="">-Narotam</h5>
 
-          <h5 className="text-center mt-4">How It Started / Our History</h5>
-          <div className="d-flex justify-content-center">
-            <div className="textbottomboder  " />
-            <div className="textbottomboder " />
-          </div>
-          <p className="mt-5 d-inline-block mb-0">
+          <h4 className="text-center mt-3 history-heading">
+            How It Started / Our History
+          </h4>
+          <div className="d-flex justify-content-center"></div>
+          <p className="mt-2 d-inline-block mb-0 info-text">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's Lorem Ipsum is simply
             dummy text of the printing and typesetting industry. Lorem Ipsum has
@@ -43,7 +47,10 @@ const OurFounder = () => {
             and typesetting industry.
           </p>
           <div className="text-end">
-            <Link to="#" className="ms-5 mt-2 text-white bg-primary px-4 py-2">
+            <Link
+              to="#"
+              className="ms-5 mt-2 text-white bg-primary btn-sm px-4 py-2"
+            >
               Read more
             </Link>
           </div>
