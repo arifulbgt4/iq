f0e97cc056d92d3b2a552865f09d46525f7c8123
sabbirlog <mridhasabbir5@gmail.com>
2021-01-18 19:11:28 +0600

    feat: steps to choose map

--- src/components/ChooseCard/index.js
+++ src/components/ChooseCard/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Card, CardBody, CardImg, CardTitle } from 'reactstrap';
 
-const ChooseCard = () => {
-  return <div>choosecard</div>;
+const ChooseCard = (props) => {
+  const { id, img, title, description } = props;
+  return (
+    <Card className="choose-card">
+      <div className="choose-card-img">
+        <CardImg top width="100%" src={img} alt="choose card image" />
+      </div>
+      <CardBody>
+        <CardTitle></CardTitle>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default ChooseCard;
