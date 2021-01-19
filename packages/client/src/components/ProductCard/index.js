17acd9605e2b828f2eb1f22f64b6f039140fc9d3
sabbirlog <mridhasabbir5@gmail.com>
2021-01-19 19:46:01 +0600

    feat: 3d effect on product card

--- packages/client/src/components/ProductCard/index.js
+++ packages/client/src/components/ProductCard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 import {
-  Card,
   CardText,
   CardBody,
   CardTitle,
@@ -8,10 +8,32 @@ import {
   Button,
 } from 'reactstrap';
 
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 50,
+  (x - window.innerWidth / 2) / 50,
+  1.1,
+];
+
+const trans = (x, y, s) =>
+  `perspective(2000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
 const ProductCard = (props) => {
   const { title, text } = props;
+  const [property, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: {
+      mass: 10,
+      tension: 350,
+      friction: 40,
+    },
+  }));
   return (
-    <Card className="product shadow">
+    <animated.div
+      className="product shadow bg-white"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: property.xys.interpolate(trans) }}
+    >
       <CardBody className="text-center">
         <CardTitle tag="h3" className="">
           Product
@@ -25,7 +47,7 @@ const ProductCard = (props) => {
           Shedule an appointment
         </Button>
       </CardBody>
-    </Card>
+    </animated.div>
   );
 };
 
