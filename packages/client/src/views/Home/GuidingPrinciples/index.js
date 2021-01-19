da5d9c55045381d1610c67ed045d776af9d4fc44
sabbirlog <mridhasabbir5@gmail.com>
2021-01-19 19:55:10 +0600

    feat: guiding-priciple title

--- packages/client/src/views/Home/GuidingPrinciples/index.js
+++ packages/client/src/views/Home/GuidingPrinciples/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Col, Container, Row } from 'reactstrap';
 import { useSelector } from 'react-redux';
+
 import GuidingPrinciples from 'src/components/GuidingPrinciples';
+import SectionTitle from 'src/components/SectionTitle';
 
 const GuidingPrinciple = () => {
   const data = useSelector((store) => store.guidings);
   return (
     <section className="guiding-principle">
       <Container>
+        <SectionTitle title="Our Guiding Principles" />
         <Row>
-          <Col md={12}>
-            <h2 className="text-center py-5">Our Guiding Principles</h2>
-          </Col>
           <Col md={12}>
             {data &&
               data.map((item, i) => <GuidingPrinciples key={i} {...item} />)}
