9bdf454140a3d44877c9525705be933565d19cb8
sunny335 <ahmedsunny868@gmail.com>
2021-01-20 10:10:26 +0600

    fix: footer icons

--- packages/client/src/components/Footer/FooterMain/index.js
+++ packages/client/src/components/Footer/FooterMain/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCube } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCube,
+  faEnvelope,
+  faLocationArrow,
+  faMapMarked,
+  faPhone,
+  faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 const data = [
   {
@@ -12,21 +19,25 @@ const data = [
         id: 1,
         name: 'Our Story',
         path: '#',
+        icons: faCube,
       },
       {
         id: 2,
         name: 'Contact Us',
         path: '#',
+        icons: faCube,
       },
       {
         id: 3,
         name: 'ZASUWA Fabrics',
         path: '#',
+        icons: faCube,
       },
       {
         id: 4,
         name: 'Product Care',
         path: '#',
+        icons: faCube,
       },
     ],
   },
@@ -37,21 +48,25 @@ const data = [
         id: 1,
         name: 'Size Reference',
         path: '#',
+        icons: faCube,
       },
       {
         id: 2,
         name: 'Shipping',
         path: '#',
+        icons: faCube,
       },
       {
         id: 3,
         name: 'Returns & Exchanges',
         path: '#',
+        icons: faCube,
       },
       {
         id: 4,
         name: 'Customer Reviews',
         path: '#',
+        icons: faCube,
       },
     ],
   },
@@ -62,16 +77,19 @@ const data = [
         id: 1,
         name: '0/A Fatulah, Narayeanganj.  Dhaka 1400, Bnagladesh.',
         path: '#',
+        icons: faLocationArrow,
       },
       {
         id: 2,
         name: '(+880) 199-250552',
         path: '#',
+        icons: faPhone,
       },
       {
         id: 3,
         name: 'support@metastock.com',
         path: '#',
+        icons: faEnvelope,
       },
     ],
   },
@@ -80,12 +98,12 @@ const FooterMain = () => {
   return (
     <Row className="mt-4">
       <Col md={4}>
-        <p className="pe-5 text-justify me-5 details">
+        <h5 className="pe-5 text-justify me-5 details text-white fw-normal">
           In publishing and graphic design, Lorem ipsum is a placeholder text
           commonly used to demonstrate the visual form of a document or a
           typeface without relying on meaningful content. Lorem ipsum may be
           used as a placeholder before final copy is available
-        </p>
+        </h5>
       </Col>
       {data &&
         data.map((items) => (
@@ -93,8 +111,11 @@ const FooterMain = () => {
             <ul className="list-unstyled p-0 m-0">
               {items.navitem &&
                 items.navitem.map((item) => (
-                  <li className="mb-3 text-capitalize" key={item.id}>
-                    <FontAwesomeIcon icon={faCube} />
+                  <li
+                    className={`mb-3 text-capitalize ${items.id > 2 && 'w-75'}`}
+                    key={item.id}
+                  >
+                    <FontAwesomeIcon icon={item.icons} />
                     <Link
                       to={item.path}
                       className="align-items-center ms-2 text-decoration-none text-capitalize"
