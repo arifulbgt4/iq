67f7317eebae1727729f286f0f373fd1ea1f98a7
heartsantor <antorboss.bd@gmail.com>
2021-01-20 17:16:15 +0600

    feat: contactus

---
+++ packages/client/src/views/ContactUs/GetInTuch/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+import Contact from 'src/assets/image/contactUs/Contact.png';
+const index = () => {
+  return (
+    <>
+      <Row>
+        <Col lg={8} md={8} sm={12}>
+          <div className="tuch-text-area">
+            <h1>Get in touch</h1>
+            <p>
+              Want to get in touch? We'd love to hear from you. Here's how you
+              can reach us...
+            </p>
+          </div>
+        </Col>
+        <Col lg={4} md={4} sm={12}>
+          <div>
+            <img src={Contact} alt="" />
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default index;
---
+++ packages/client/src/views/ContactUs/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import GetInTuch from './GetInTuch';
+const index = () => {
+  return (
+    <Container>
+      <GetInTuch />
+    </Container>
+  );
+};
+
+export default index;
