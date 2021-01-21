93c04348881f385991dfbb88eb322a628375e1ef
sunny335 <ahmedsunny868@gmail.com>
2021-01-21 23:03:40 +0600

    fix: slider padding

--- packages/client/src/views/Home/ContactUs/index.js
+++ packages/client/src/views/Home/ContactUs/index.js
@@ -7,7 +7,7 @@ import ContactForm from './ContactForm';
 const ContactUs = () => {
   return (
     <Row className="my-5 align-items-center">
-      <Col md={6}>
+      <Col md={6} className="my-4 my-md-0">
         <img src={contactimg} alt="img" className="img-fluid" />
       </Col>
       <Col md={6} className="px-5">
