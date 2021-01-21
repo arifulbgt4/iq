600e9770154d600b0c57e91dc10898c164431346
sunny335 <ahmedsunny868@gmail.com>
2021-01-21 22:53:47 +0600

    fix: contact us  responsive

--- packages/client/src/views/Home/ContactUs/index.js
+++ packages/client/src/views/Home/ContactUs/index.js
@@ -7,10 +7,10 @@ import ContactForm from './ContactForm';
 const ContactUs = () => {
   return (
     <Row className="my-5 align-items-center">
-      <Col md={6}>
+      <Col lg={6} md={6} className="my-4 my-lg-0">
         <img src={contactimg} alt="img" className="img-fluid" />
       </Col>
-      <Col md={6} className="px-5">
+      <Col lg={6} md={6} className="px-2 px-lg-5">
         <p className="fs-1 text-primary">
           Have doubts? Don't Worry just <b>Contact Us.</b>
         </p>
