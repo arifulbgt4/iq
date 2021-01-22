38db22bdab8be6d1ce7673dbff5abcd06a1a8949
sunny335 <ahmedsunny868@gmail.com>
2021-01-22 08:24:58 +0600

    fix: contact us heading responsive

--- packages/client/src/views/ContactUs/GetInTuch/index.js
+++ packages/client/src/views/ContactUs/GetInTuch/index.js
@@ -4,9 +4,9 @@ import Contact from 'src/assets/image/contactUs/Contact.png';
 const index = () => {
   return (
     <>
-      <Row>
+      <Row className="align-items-center text-center text-md-start mt-4 mt-md-0">
         <Col lg={8} md={8} sm={12}>
-          <div className="tuch-text-area h-100 d-flex align-items-center">
+          <div className="tuch-text-area d-flex py-5 py-md-0">
             <div>
               <h1>Get in touch</h1>
               <p>
@@ -16,9 +16,9 @@ const index = () => {
             </div>
           </div>
         </Col>
-        <Col lg={4} md={4} sm={12}>
+        <Col lg={4} md={4} sm={12} className="d-none d-sm-flex">
           <div>
-            <img src={Contact} alt="contact" />
+            <img src={Contact} alt="contact img-fluid" />
           </div>
         </Col>
       </Row>
