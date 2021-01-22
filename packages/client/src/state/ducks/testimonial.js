00262e836c2831c17dab6a72be81035928896a06
sunny335 <ahmedsunny868@gmail.com>
2021-01-22 08:15:11 +0600

    fix: testimonail slider responsive

--- packages/client/src/state/ducks/testimonial.js
+++ packages/client/src/state/ducks/testimonial.js
@@ -4,7 +4,7 @@ const data = [
   {
     id: 1,
     img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
-    name: 'Airin Sultana',
+    name: 'Airin Fatema',
     text:
       '“As a student with only a few savings in my account, I needed the right platform and path to invest my money. With Intel Quest, I knew I had reached my destination. The perfect guidance combined with the application of the best trading tools have made me quite self-sufficient in trading, thanks to Intel Quest.”',
   },
@@ -12,7 +12,7 @@ const data = [
     id: 2,
     img:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRAf9gA-CRlCv5tHlzbNDJnmqvnNG1jRrHGA&usqp=CAU',
-    name: 'Airin Sultana',
+    name: 'Ahmed Sunny',
     text:
       '“As a student with only a few savings in my account, I needed the right platform and path to invest my money. With Intel Quest, I knew I had reached my destination. The perfect guidance combined with the application of the best trading tools have made me quite self-sufficient in trading, thanks to Intel Quest.”',
   },
@@ -20,7 +20,7 @@ const data = [
     id: 3,
     img:
       'https://lavinephotography.com.au/wp-content/uploads/2017/01/PROFILE-Photography-112.jpg',
-    name: 'Airin Sultana',
+    name: 'Airin Lubna',
     text:
       '“As a student with only a few savings in my account, I needed the right platform and path to invest my money. With Intel Quest, I knew I had reached my destination. The perfect guidance combined with the application of the best trading tools have made me quite self-sufficient in trading, thanks to Intel Quest.”',
   },
