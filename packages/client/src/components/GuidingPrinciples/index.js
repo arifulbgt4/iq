7b67ded0a4fd68dfbcdb2888db3eadb38a6f0c5f
sabbirlog <mridhasabbir5@gmail.com>
2021-01-22 21:13:17 +0600

    fix: our guiding priciple responsive bug

--- packages/client/src/components/GuidingPrinciples/index.js
+++ packages/client/src/components/GuidingPrinciples/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 const GuidingPrinciples = (props) => {
   const { img, title, description, style } = props;
   return (
-    <div className={`mx-2 d-flex ${style} mb-5`}>
+    <div className={`guiding-principle-card mx-2 d-flex ${style} mb-5`}>
       <div className="d-flex h-100 align-items-center card-width">
         <img className="px-4 custrom-img " src={img} alt="idea" />
-        <div className="text-capitalize ms-3">
+        <div className="guiding-principle-card_description text-capitalize ms-3">
           <h2 className="fw-bold">{title}</h2>
           <p>{description}</p>
         </div>
