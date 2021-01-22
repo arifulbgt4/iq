655e7033a18a8e2168b0af6250f37e256a9f9793
ArafatShuvo <5arafatshovo@gmail.com>
2021-01-22 19:10:38 +0600

    feat: made header responsive

--- packages/client/src/components/Header/index.js
+++ packages/client/src/components/Header/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Button, Col, Container, Nav, Navbar, NavItem, Row } from 'reactstrap';
 import logo from 'src/assets/image/logo.png';
+import MenuBar from './MenuBar';
 
 const Header = () => {
   return (
     <header className="border-bottom shadow-sm py-4">
       <Container>
         <Row className=" align-items-center">
-          <Col md={12} lg={3} className="d-flex align-items-center hlogo">
-            <img src={logo} alt="logo" />
-            <Link to="/" className="mb-0 h4 text-decoration-none text-dark">
-              <span className="text-primary">Exel</span>Stock
-            </Link>
+          <Col md={12} lg={3} sm={12} className="hiconclm">
+            <div className="d-flex align-items-center hlogo">
+              <img src={logo} alt="logo" />
+              <Link to="/" className="mb-0 h4 text-decoration-none text-dark">
+                <span className="text-primary">Exel</span>Stock
+              </Link>
+              <MenuBar />
+            </div>
           </Col>
 
           <Col md={12} lg={6} className="hnav">
