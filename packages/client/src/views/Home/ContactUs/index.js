cf651b807c06f206308f3c709eafe8460351f602
sunny335 <ahmedsunny868@gmail.com>
2021-01-24 20:50:00 +0600

    fix: contact us form padding

--- packages/client/src/views/Home/ContactUs/index.js
+++ packages/client/src/views/Home/ContactUs/index.js
@@ -11,7 +11,7 @@ const ContactUs = () => {
         <Col lg={6} md={12} className="my-4 my--0">
           <img src={contactimg} alt="img" className="img-fluid" />
         </Col>
-        <Col lg={6} md={12} className="px-2 px-lg-5">
+        <Col lg={6} md={12} className="px-3 px-sm-0 px-md-5 px-lg-3 px-xl-5">
           <p className="fs-1">
             Have doubts? Don't Worry just <b>Contact Us.</b>
           </p>
