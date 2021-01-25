e305996eb6c0b3313fac68af0e848442bd748599
sunny335 <ahmedsunny868@gmail.com>
2021-01-25 21:04:07 +0600

    fix: title animation

--- packages/client/src/components/SectionTitle/index.js
+++ packages/client/src/components/SectionTitle/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Col, Row } from 'reactstrap';
+import { Parallax } from 'react-skrollr';
 
+const scroll = {
+  data: {
+    'data-top-bottom': 'transform: scale(1.5);',
+    'data-center-center': 'opacity: 1;',
+    'data-bottom-top': 'opacity: 0;transform: scale(1);',
+  },
+};
 const SectionTitle = (props) => {
   const { color, title, description, border = true } = props;
   return (
     <Row className="section-title">
       <Col md={12}>
-        <h1
-          className={`text-center text-capitalize fw-bold ${
-            border ? 'position-relative border-line' : 'mb-0 pb-2'
-          } ${description ? 'heading' : 'mb-3 mb-sm-5'} text-${color}`}
-        >
-          {title}
-        </h1>
+        <Parallax data={scroll.data}>
+          <h1
+            className={`text-center text-capitalize fw-bold ${
+              border ? 'position-relative border-line' : 'mb-0 pb-2'
+            } ${description ? 'heading' : 'mb-3 mb-sm-5'} text-${color}`}
+          >
+            {title}
+          </h1>
+        </Parallax>
       </Col>
 
       {description && (
