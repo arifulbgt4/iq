2426b375d96b04b23a0a43b1f3671e8b294ce6e7
sunny335 <ahmedsunny868@gmail.com>
2021-01-25 15:49:55 +0600

    fix:  react paralex scroll

--- packages/client/src/views/Home/GuidingPrinciples/index.js
+++ packages/client/src/views/Home/GuidingPrinciples/index.js
@@ -1,10 +1,24 @@
 import React, { useEffect } from 'react';
 import { Col, Container, Row } from 'reactstrap';
 import { useSelector, useDispatch } from 'react-redux';
+import { ParallaxProvider, Parallax } from 'react-skrollr';
 
 import { fatchGuid } from 'src/state/ducks/guidings';
 import GuidingPrinciples from 'src/components/GuidingPrinciples';
 import SectionTitle from 'src/components/SectionTitle';
+
+const scroll = {
+  left: {
+    'data-top-bottom': 'transform: translateX(-50%);',
+    'data-center-center': 'opacity: 1;',
+    'data-bottom-top': 'opacity: 0;transform: translateX(0%);',
+  },
+  right: {
+    'data-top-bottom': 'transform: translateX(50%);',
+    'data-center-center': 'opacity: 1;',
+    'data-bottom-top': 'opacity: 0;transform: translateX(0%);',
+  },
+};
 
 const GuidingPrinciple = () => {
   const { data, loading } = useSelector((store) => store.guidings);
@@ -19,29 +33,42 @@ const GuidingPrinciple = () => {
     return 'Loadding';
   }
   return (
-    <section className="guiding-principle">
-      <Container>
-        <SectionTitle title="Our Guiding Principles" border={false} />
-        <Row>
-          <Col md={12}>
-            {data &&
-              data.map((item) => {
-                const image = process.env.API_URL + item.image.url;
+    <ParallaxProvider
+      init={{
+        smoothScrollingDuration: 1000,
+        smoothScrolling: true,
+        forceHeight: false,
+      }}
+      getScrollTop={(scrollTop) => console.log('scrollTop', scrollTop)}
+    >
+      <section className="guiding-principle">
+        <Container>
+          <SectionTitle title="Our Guiding Principles" border={false} />
+          <Row>
+            <Col md={12}>
+              {data &&
+                data.map((item) => {
+                  const image = process.env.API_URL + item.image.url;
 
-                return (
-                  <GuidingPrinciples
-                    key={item.id}
-                    image={image}
-                    title={item.title}
-                    description={item.description}
-                    right={item.id % 2 === 0}
-                  />
-                );
-              })}
-          </Col>
-        </Row>
-      </Container>
-    </section>
+                  return (
+                    <Parallax
+                      data={item.id % 2 === 0 ? scroll.left : scroll.right}
+                    >
+                      <GuidingPrinciples
+                        key={item.id}
+                        image={image}
+                        title={item.title}
+                        description={item.description}
+                        right={item.id % 2 === 0}
+                      />
+                    </Parallax>
+                  );
+                })}
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </ParallaxProvider>
   );
 };
 
