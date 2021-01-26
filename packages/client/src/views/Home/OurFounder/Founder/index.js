2c203049c2aff04f02c4740945bba8cfc90043cc
sunny335 <ahmedsunny868@gmail.com>
2021-01-26 12:00:54 +0600

    fix: our founder animation

--- packages/client/src/views/Home/OurFounder/Founder/index.js
+++ packages/client/src/views/Home/OurFounder/Founder/index.js
@@ -11,13 +11,13 @@ const trans = (x, y, s) =>
 
 const scroll = {
   data: {
-    'data-top-bottom': 'opacity: 1; transform: translate(10%, -50%);',
-    'data-center-center': 'opacity: 1;',
+    'data-top-bottom': 'opacity: 1; ',
+    'data-center-center': 'opacity: 1; transform: translate(0%, 0%);',
     'data-bottom-top': 'opacity: 0;transform: translate(-10%, 20%);',
   },
   data2: {
-    'data-top-bottom': 'opacity: 1; transform: translate(-5%, 0%);',
-    'data-center-center': 'opacity: 1;',
+    'data-top-bottom': 'opacity: 1; ',
+    'data-center-center': 'opacity: 1; transform: translate(-5%, 0%);',
     'data-bottom-top': 'opacity: 0;transform: translate(10%, 0%);',
   },
   data3: {
