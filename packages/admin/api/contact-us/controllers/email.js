e4395b7a89c6edec549c79526021c1fc628b19e9
Ariful islam <arifulbgt4@gmail.com>
2021-01-27 05:49:46 +0600

    feat: email send to direct mail

---
+++ packages/admin/api/contact-us/controllers/email.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+  send: async (ctx) => {
+    const options = ctx.request.body;
+    try {
+      await strapi.plugins.email.services.email.send({
+        to: env('SENDGRID_TO_MAIL'),
+        from: options.email,
+        subject: options.subject,
+        text: options.message,
+      });
+    } catch (e) {
+      if (e.statusCode === 400) {
+        return ctx.badRequest(e.message);
+      }
+      throw new Error(`Couldn't send email: ${e.message}.`);
+    }
+
+    // Send 200 `ok`
+    ctx.send('Email sent');
+  },
+});
