aaac472b573d92cb139ba497f20ca4d19599ef79
sunny335 <ahmedsunny868@gmail.com>
2021-01-26 11:11:16 +0600

    fix: product animation

--- packages/client/src/views/Home/OurProducts/index.js
+++ packages/client/src/views/Home/OurProducts/index.js
@@ -8,11 +8,11 @@ import ProductCard from 'src/components/ProductCard';
 import SectionTitle from 'src/components/SectionTitle';
 
 const scroll = {
-data: {
-    'data-top-bottom': 'transform: translateY(-20%);',
-    'data-center-center': 'opacity: 1;',
-    'data-bottom-top': 'opacity: 0;transform: translateY(10%);',
-  }
+  data: {
+    'data-top-bottom': '',
+    'data-center-center': 'opacity: 1; transform: translate(0%, -100%);',
+    'data-bottom-top': 'opacity: 1;transform: translate(0%, 100%);',
+  },
 };
 
 const OurProducts = () => {
@@ -37,37 +37,35 @@ const OurProducts = () => {
       <Container>
         <SectionTitle color="white" title={productsHeader?.title} />
         <Row className="mt-0 mt-sm-3 product-row">
-          <Col
-            xl={{ size: 10, offset: 1 }}
-            lg={12}
-            md={{ size: 12, offset: 0 }}
-            sm={{ size: 10, offset: 1 }}
-            xs={12}
-          >
-            <Row className="justify-content-center">
-              {data &&
-                data.map((item) => (
-                  <Col
-                    key={item.id}
-                    xl={4}
-                    lg={6}
-                    md={6}
-                    sm={12}
-                    className="product-item pb-4"
-                  >
-                    <Parallax
-                      data={scroll.data}
+          <Parallax data={scroll.data}>
+            <Col
+              xl={{ size: 10, offset: 1 }}
+              lg={12}
+              md={{ size: 12, offset: 0 }}
+              sm={{ size: 10, offset: 1 }}
+              xs={12}
+            >
+              <Row className="justify-content-center">
+                {data &&
+                  data.map((item) => (
+                    <Col
+                      key={item.id}
+                      xl={4}
+                      lg={6}
+                      md={6}
+                      sm={12}
+                      className="product-item pb-4"
                     >
                       <ProductCard
                         name={item.name}
                         title={item.title}
                         description={item.description}
                       />{' '}
-                    </Parallax>
-                  </Col>
-                ))}
-            </Row>
-          </Col>
+                    </Col>
+                  ))}
+              </Row>
+            </Col>
+          </Parallax>
         </Row>
       </Container>
     </section>
