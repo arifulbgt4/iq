7899a42f754fd6c1d0181ffb773fed6af7eae093
sunny335 <ahmedsunny868@gmail.com>
2021-01-26 20:01:26 +0600

    fix: map animation

--- packages/client/src/views/ContactUs/MapLocation/index.js
+++ packages/client/src/views/ContactUs/MapLocation/index.js
@@ -5,9 +5,9 @@ import { Parallax } from 'react-skrollr';
 
 const scroll = {
   data: {
-    'data-top-bottom': 'opacity: 1; transform: translate(0, 0%);',
+    'data-top-bottom': 'opacity: 1; transform: translate(0, -10%);',
     'data-center-center': 'opacity: 1; ',
-    'data-bottom-top': 'opacity: 0;transform: translate(0, 70%);',
+    'data-bottom-top': 'opacity: 0;transform: translate(0, 50%);',
   },
 };
 
