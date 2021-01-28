c38f9eba6b94a69a5c6c94c34a5be79534f30d44
Ariful islam <arifulbgt4@gmail.com>
2021-01-28 10:59:33 +0600

    fix: contact us image animation

--- packages/client/src/views/Home/ContactUs/index.js
+++ packages/client/src/views/Home/ContactUs/index.js
@@ -7,8 +7,8 @@ import ContactForm from 'src/components/ContactForm';
 
 const scroll = {
   data2: {
-    'data-top-bottom': 'opacity: 1; transform: translate(20%, 0%);',
-    'data-center-center': 'opacity: 0.7;',
+    'data-top-bottom': 'opacity: 1;',
+    'data-center-center': 'opacity: 1;transform: translate(0, 0)',
     'data-bottom-top': 'opacity: 0;transform: translate(-20%, 0%);',
   },
 };
@@ -18,10 +18,10 @@ const ContactUs = () => {
     <Container className="contact">
       <Row className="my-5 align-items-center">
         <Col lg={6} md={12} className="my-4 my--0">
-         <Parallax data={scroll.data2}>
-          <img src={contactimg} alt="img" className="img-fluid" />
+          <Parallax data={scroll.data2}>
+            <img src={contactimg} alt="img" className="img-fluid" />
           </Parallax>
-          </Col>
+        </Col>
         <ContactForm />
       </Row>
     </Container>
