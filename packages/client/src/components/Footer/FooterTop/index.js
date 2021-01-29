d95bdee41f033b9a97ebe2da9596c7e963d30709
Ariful islam <arifulbgt4@gmail.com>
2021-01-29 12:19:29 +0600

    chore(feature): remove language from footer

--- packages/client/src/components/Footer/FooterTop/index.js
+++ packages/client/src/components/Footer/FooterTop/index.js
@@ -14,24 +14,11 @@ const FooterTop = () => {
       <Col
         xl={4}
         lg={12}
-        className="d-flex justify-content-between justify-content-lg-center justify-content-xl-start mb-0 mb-md-3 mb-xl-0"
+        className="d-flex justify-content-center justify-content-lg-center justify-content-xl-start mb-0 mb-md-3 mb-xl-0"
       >
         <h4 className="mb-0 text-white ">
           <img width={50} src={logo} alt="logo" />
         </h4>
-        <div className="list-unstyled d-flex d-lg-none align-items-center me-0 me-md-5 pe-0 pe-md-4">
-          <FontAwesomeIcon icon={faCubes} style={{ fontSize: 25 }} />
-          <Input
-            type="select"
-            name="select"
-            className="bg-transparent text-white border-0 ps-1"
-          >
-            <option>English(USA)</option>
-            <option>English(USA)</option>
-            <option>English(USA)</option>
-            <option>English(USA)</option>
-          </Input>
-        </div>
       </Col>
       <Col md={12} lg={12} xl={8} className="d-none d-md-block">
         <ul className="d-flex justify-content-between align-items-center m-0 p-0">
@@ -43,20 +30,6 @@ const FooterTop = () => {
           </li>
           <li className="list-unstyled me-5 me-lg-0">
             <h5 className="text-white me-5 me-lg-0"> Contact US</h5>
-          </li>
-
-          <li className="list-unstyled d-none d-lg-flex align-items-center">
-            <FontAwesomeIcon icon={faCubes} style={{ fontSize: 25 }} />
-            <Input
-              type="select"
-              name="select"
-              className="bg-transparent text-white border-0 ps-1"
-            >
-              <option>English(USA)</option>
-              <option>English(USA)</option>
-              <option>English(USA)</option>
-              <option>English(USA)</option>
-            </Input>
           </li>
         </ul>
       </Col>
