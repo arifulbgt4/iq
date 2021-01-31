57e995b23e475f6ba4426457bbc44339ea5b7aff
Ariful islam <arifulbgt4@gmail.com>
2021-02-01 03:33:44 +0600

    feat: calendly api

--- packages/client/src/components/HeroContent/index.js
+++ packages/client/src/components/HeroContent/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 
+import Calendly from 'src/components/Calendly';
 import LergerText from '../LergerText';
 
 const HeroContent = (props) => {
   const { title, description } = props;
+  const url = 'https://calendly.com/arifulbgt4/15min';
+
   return (
     <div className="banner-content text-center">
       <LergerText text={title} maxLength={23} />
       <p className="banner-content-description text-capitalize">
         {description}
       </p>
-      <Button className="rounded-pill px-4" color="primary">
-        Shedule an appointment
-      </Button>
+      <Calendly url={url} />
     </div>
   );
 };
